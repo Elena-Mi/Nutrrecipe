f5ec5049ede725e1b4b32e4af756aa54b2fef271
Elena Mitrofanova <tang.aloyna@gmail.com>
2024-03-08 12:38:10 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [advice, setAdvice] = useState("")
+
+  useEffect( () => {
+    const getAdvice = async () => {
+      const response = await fetch(` https://api.quotable.io/random `);
+      const data = await response.json();
+      setAdvice(data.content)
+      
+
+    }
+    getAdvice()
+  },[])
+  
+  
+
+
+
+ 
+	return (
+		<div>
+       <p>{advice}</p>
+       <button onClick={() => setAdvice}>Show next</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
